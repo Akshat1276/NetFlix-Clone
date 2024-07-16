eee7fd5898a3f15087ede49e910948535cb67222
Akshat Yadav <akshatyadav1207@gmail.com>
2024-07-16 23:40:23 +0530

    update

--- netflix_home_page/src/Components/Navbar/Navbar.jsx
+++ netflix_home_page/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <div className="navbar-left"></div>
-        <div className="navbar-left"></div>
+        <div className="navbar-left">
+            <img src={logo}/>
+            <ul>
+                <li>Home</li>
+                <li>TV Shows</li>
+                <li>Movies</li>
+                <li>New & Popular</li>
+                <li>My List</li>
+                <li>Browse by Languages</li>
+            </ul>
+        </div>
+        <div className="navbar-right">
+            <img src={search_icon} alt="" className='icons'/>
+            <p>Children</p>
+            <img src={bell_icon} alt="" className='icons'/>
+        </div>
     </div>
   )
 }
