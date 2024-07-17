82f39bf2cad944ef5df52a04de17a03f82cf06e6
Akshat Yadav <akshatyadav1207@gmail.com>
2024-07-17 22:37:04 +0530

    Update Footer.jsx

--- netflix_home_page/src/Components/Footer/Footer.jsx
+++ netflix_home_page/src/Components/Footer/Footer.jsx
@@ -5,11 +5,15 @@ import twitter_icon from '../../assets/twitter_icon.png'
 import instagram_icon from '../../assets/instagram_icon.png'
 import facebook_icon from '../../assets/facebook_icon.png'
 
-
 const Footer = () => {
   return (
     <div className='footer'>
-        
+      <div className="footer-icons">
+        <img src={facebook_icon} alt="" />
+        <img src={youtube_icon} alt="" />
+        <img src={instagram_icon} alt="" />
+        <img src={twitter_icon} alt="" />
+      </div>
     </div>
   )
 }
