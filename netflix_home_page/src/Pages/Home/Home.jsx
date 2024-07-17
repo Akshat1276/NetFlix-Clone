0bca5b2314bf6aa5907778b1dccf3d1ed6d38a87
Akshat Yadav <akshatyadav1207@gmail.com>
2024-07-17 22:49:44 +0530

    a

--- netflix_home_page/src/Pages/Home/Home.jsx
+++ netflix_home_page/src/Pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import hero_banner from '../../assets/hero_banner.jpg'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
 import TitleCards from '../../Components/TitleCards/TitleCards'
+import Footer from '../../Components/Footer/Footer'
 
 
 const Home = () => {
@@ -30,6 +31,7 @@ const Home = () => {
           <TitleCards title={"Upcoming"}/>
           <TitleCards title={"Top Picks for You"}/>
         </div>
+        <Footer/>
     </div>
   )
 }
