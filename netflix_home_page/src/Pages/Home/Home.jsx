25d87605ea2892aa3afe837c930e4355047ef681
Akshat Yadav <akshatyadav1207@gmail.com>
2024-07-17 22:08:37 +0530

    Update Home.jsx

--- netflix_home_page/src/Pages/Home/Home.jsx
+++ netflix_home_page/src/Pages/Home/Home.jsx
@@ -24,6 +24,12 @@ const Home = () => {
             <TitleCards/>
           </div>
         </div>
+        <div className="more-cards">
+          <TitleCards/>
+          <TitleCards/>
+          <TitleCards/>
+          <TitleCards/>
+        </div>
     </div>
   )
 }
