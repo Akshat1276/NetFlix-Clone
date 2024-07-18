c68e939bf44a64511451e7da430908217c464ee7
Akshat Yadav <akshatyadav1207@gmail.com>
2024-07-18 22:44:59 +0530

    Update Player.jsx

--- netflix_home_page/src/Pages/Player/Player.jsx
+++ netflix_home_page/src/Pages/Player/Player.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useParams } from 'react-router-dom'
 
 const Player = () => {
+  const {id} = useParams();
 
   const [apiData, setApiData] = useState({
     name: "",
@@ -20,9 +22,9 @@ const Player = () => {
   };
   
   useEffect(()=>{
-    fetch('https://api.themoviedb.org/3/movie/1022789/videos?language=en-US', options)
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
     .then(response => response.json())
-    .then(response => setApiData(response.results))
+    .then(response => setApiData(response.results[0]))
     .catch(err => console.error(err));
   },[])
 
@@ -31,11 +33,11 @@ const Player = () => {
   return (
     <div className='player'>
         <img src={back_arrow_icon} alt="" />
-        <iframe width='90%' height='90%' src='https://www.youtube.com/embed/SHT0y9Gq_rk' title='trailer' frameBorder='0' allowFullScreen></iframe>
+        <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`} title='trailer' frameBorder='0' allowFullScreen></iframe>
         <div className="player-info">
-          <p>Published Date</p>
-          <p>Name</p>
-          <p>Type</p>
+          <p>{apiData.published_at.slice(0, 10)}</p>
+          <p>{apiData.name}</p>
+          <p>{apiData.type}</p>
         </div>
     </div>
   )
