147bf8e6ea64a91524489294879be51d5a4ba668
Akshat Yadav <akshatyadav1207@gmail.com>
2024-07-19 12:57:51 +0530

    Update firebase.js

--- netflix_home_page/src/firebase.js
+++ netflix_home_page/src/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth"
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut, signOut } from "firebase/auth"
+import { addDoc, getFirestore, collection } from "firebase/firestore"
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBCzJlMDPZxaziRS-kswFKvYDAp_OsITzQ",
@@ -17,3 +19,33 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+const signup = async(name, email, password)=>{
+    try{
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await addDoc(collection(db, "user"), {
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        });
+    } catch(error){
+        console.log(error);
+        alert(error);
+    }
+}
+const login = async (email, password)=>{
+    try{
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+const logout = async ()=>{
+    signOut(auth);
+}
+
+export {auth, db, login, signup, logout};
